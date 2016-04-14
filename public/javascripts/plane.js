f1b03f533874ac2e5caf73c50acbc5e8b2fdf35f
Kelly Everett <dkever79@yahoo.com>
2016-04-14 12:33:58 -0500

    fixed animation

--- public/javascripts/plane.js
+++ public/javascripts/plane.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
 
   function balloonLeft() {
     if($(window).width() > 800){
-      $("#ballon").animate({left: "-=800"}, 8000, "swing", balloonRight);
+      $("#balloon").animate({left: "-=800"}, 8000, "swing", balloonRight);
     }else{
-      $("#ballon").animate({left: "-=200"}, 8000, "swing", balloonRight);
+      $("#balloon").animate({left: "-=200"}, 8000, "swing", balloonRight);
     }
   }
-  function planeRight() {
+  function balloonRight() {
     if($(window).width() > 800){
-      $("#ballon").animate({left: "+=800"}, 8000, "swing", balloonLeft);
+      $("#balloon").animate({left: "+=800"}, 8000, "swing", balloonLeft);
     }else{
-      $("#ballon").animate({left: "+=200"}, 8000, "swing", balloonLeft);
+      $("#balloon").animate({left: "+=200"}, 8000, "swing", balloonLeft);
     }
   }
   balloonRight();
